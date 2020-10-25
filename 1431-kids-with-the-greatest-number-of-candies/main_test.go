368cd60d88e50dbfa46b1b4b451fdd577a34f59b
Nurbek Chymbaev <nurbek188@gmail.com>
2020-10-25 17:35:40 +0300

    1431-kids-with-the-greatest-number-of-candies

---
+++ 1431-kids-with-the-greatest-number-of-candies/main_test.go
@@ -0,0 +1,10 @@
+package _431_kids_with_the_greatest_number_of_candies
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestKidsWithCandies(t *testing.T) {
+	fmt.Println(kidsWithCandies([]int{2, 3, 5, 1, 3}, 3))
+}
